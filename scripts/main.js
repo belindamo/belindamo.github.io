17b295c4c37d73ec36f599cccbdcd6cf644aa275
Belinda <belindamomo@gmail.com>
2016-11-24 09:52:41 -0800

    Moved bmos travels to this repository

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+//image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc == 'images/header.jpg') {
+		myImage.setAttribute('src', 'images/chamounix.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/header.jpg');
+	}
+}
+
+// personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h3');
+
+function setUserName() {
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name', myName); /* like an array*/
+	while (localStorage.getItem('name')=='null') {
+		var myName = prompt('Please enter your name');
+	}
+	myHeading.textContent = 'Hey there ' + myName + '! Where are you in the world?';
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Hey there ' + storedName + '! Where are you in the world?';	
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
+
